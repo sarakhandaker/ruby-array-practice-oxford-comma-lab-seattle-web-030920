facf381521cd0e13ac7ebd5b13b685e0ba8eaa45
SaraKhandaker <kttsara@gmail.com>
2020-08-05 16:04:11 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size==1
+        array[0]
+    elsif array.size==2
+        array.join(" and ")
+    else
+    last_element=array.pop()
+    array.push("and")
+array.join(', ')+" " +last_element
+    end
 end
